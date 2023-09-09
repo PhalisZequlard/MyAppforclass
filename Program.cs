75b2993404d18a5cbcf3f2d72916547494eb386c
PCovered <dereckyao1006@gmail.com>
2023-09-09 12:24:18 +0800

    UPDATE

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+string afriend = "YEEET";
+string bfriend = "YeeEET";
+Console.WriteLine($"Hello {afriend} and {bfriend}");
+Console.WriteLine($"frienda: {afriend} has {afriend.Length} characters");
+Console.WriteLine($"friendb: {bfriend} has {bfriend.Length} characters");
