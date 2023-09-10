e10c44aed5d22d138cdb116b571f1222eb9bb9f9
PCovered <dereckyao1006@gmail.com>
2023-09-10 12:42:37 +0800

    UPDATE

--- Program.cs
+++ Program.cs
@@ -5,3 +5,27 @@ string bfriend = "YeeEET";
 Console.WriteLine($"Hello {afriend} and {bfriend}");
 Console.WriteLine($"frienda: {afriend} has {afriend.Length} characters");
 Console.WriteLine($"friendb: {bfriend} has {bfriend.Length} characters");
+
+Console.WriteLine("Hello, World!");
+string afriend = "YEEET";
+string bfriend = "YeeEET";
+Console.WriteLine($"Hello {afriend} and {bfriend}");
+Console.WriteLine($"frienda: {afriend} has {afriend.Length} characters");
+Console.WriteLine($"friendb: {bfriend} has {bfriend.Length} characters");
+
+
+string greeting = "   Hello World!   ";
+Console.WriteLine($"[{greeting}]");
+string trimmedGreeting = greeting.TrimStart();
+Console.WriteLine($"[{trimmedGreeting}]");
+trimmedGreeting = greeting.TrimEnd();
+Console.WriteLine($"[{trimmedGreeting}]");
+trimmedGreeting = greeting.Trim();
+Console.WriteLine($"[{trimmedGreeting}]");
+
+
+string sayHello = "Hello lllllll!";
+Console.WriteLine(sayHello);
+
+sayHello = sayHello.Replace("Hello", "Greetings");
+Console.WriteLine(sayHello);
