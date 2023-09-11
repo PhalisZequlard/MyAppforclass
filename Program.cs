7d9b32e79f852a13ada4c1392f8951c2101e01ab
PCovered <dereckyao1006@gmail.com>
2023-09-11 13:09:45 +0800

    UPDATE

--- Program.cs
+++ Program.cs
@@ -1,4 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+Console.WriteLine("input a number");
+int num = Convert.ToInt32(Console.ReadLine());
+for (int i=1;i<=9;i++) {
+  for (int j=1;j<=i;j++) {
+    Console.Write($"{i}x{j}={i*j} ");
+  }
+  Console.WriteLine();
+}
+
+
+// See https://aka.ms/new-console-template for more information
 // Console.WriteLine("Hello, World!");
 // string afriend = "YEEET";
 // string bfriend = "YeeEET";
@@ -31,6 +42,6 @@
 // Console.WriteLine(sayHello);
 
 
-string songLyrics = "You say goodbye, and I say hello";
-Console.WriteLine(songLyrics.Contains("goodbye"));
-Console.WriteLine(songLyrics.Contains("greetings"));
+// string songLyrics = "You say goodbye, and I say hello";
+// Console.WriteLine(songLyrics.Contains("goodbye"));
+// Console.WriteLine(songLyrics.Contains("greetings"));
