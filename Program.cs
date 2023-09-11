918507072b9b5fea74bdba930e41497f61056923
PCovered <dereckyao1006@gmail.com>
2023-09-11 13:45:32 +0800

    UPDATE

--- Program.cs
+++ Program.cs
@@ -1,12 +1,33 @@
-﻿
-Console.WriteLine("input a number");
-int num = Convert.ToInt32(Console.ReadLine());
-for (int i=1;i<=9;i++) {
-  for (int j=1;j<=i;j++) {
-    Console.Write($"{i}x{j}={i*j} ");
+﻿// Console.WriteLine("input a number");
+// int num = Convert.ToInt32(Console.ReadLine());
+// for (int i=1;i<=9;i++) {
+//   for (int j=1;j<=i;j++) {
+//     Console.Write($"{i}x{j}={i*j} ");
+//   }
+//   Console.WriteLine();
+// }
+
+using System.Drawing;
+using System.Reflection.Metadata.Ecma335;
+
+namespace ConsoleAPP1 {
+  internal class Program {
+    private static void Main(string[] args) {
+      List<Drink> drinks = new List<Drink>();
+      // Drink drink1 = new Drink() { Name = "Coke", Size = 500, Price = 10,};
+      // drinks.Add(drink1);
+      drinks.Add(new Drink() { Name = "Coke", Size = 1, Price = 10,});
+      drinks.Add(new Drink() { Name = "juice", Size = 2, Price = 20,});
+      drinks.Add(new Drink() { Name = "coffee", Size = 3, Price = 30,});
+      drinks.Add(new Drink() { Name = "Coke", Size = 4, Price = 40,});
+    }
+    
   }
-  Console.WriteLine();
 }
+
+
+
+
 
 
 // See https://aka.ms/new-console-template for more information
