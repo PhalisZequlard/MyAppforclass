7659880a90c4b61651c891bf1ca6ed95a74ccf00
PCovered <dereckyao1006@gmail.com>
2023-10-30 13:34:04 +0800

    UPDATE

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+    }
+}
